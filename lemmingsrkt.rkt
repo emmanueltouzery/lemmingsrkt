b29713ca99d32b57bb9cf7ae8cba02148566da6a
Emmanuel Touzery <etouzery@gmail.com>
2019-07-23 21:09:28 +0200

    tretji dan

--- lemmingsrkt.rkt
+++ lemmingsrkt.rkt
@@ -16,25 +16,34 @@
   (lemming-anim-info 0 2 3))
 
 (define lemming-anim (bitmap/file "lemming_anim.png"))
+(define map (bitmap/file "map.png"))
 
 (struct stanje-igre
-  (anim-korak lemming-akcija) #:transparent)
+  (anim-korak lemming-akcija lemming-x lemming-y) #:transparent)
 
-(define/match* (animiraj (stanje-igre korak akcija))
-  (scale 7
-         (crop
+(define/match* (animiraj (stanje-igre korak akcija lemming-x lemming-y))
+  (define lemming
+    (crop
           (* 16 (+ (lemming-anim-info-start-x akcija) korak))
           (* 16 (lemming-anim-info-start-y akcija))
           16
           16
-          lemming-anim)))
+          lemming-anim))
+  (scale 2
+         (place-image/align
+          lemming
+          lemming-x lemming-y "left" "bottom"
+          map)))
 
-(define/match* (naslednji (stanje-igre korak akcija))
+(define/match* (naslednji (stanje-igre korak akcija lemming-x lemming-y))
+  (define dotaknil-tla (> lemming-y 78))
   (stanje-igre
    (if (>= korak (lemming-anim-info-korake akcija)) 0 (+ 1 korak))
-   akcija))
+   (if dotaknil-tla hodi pada)
+   (if dotaknil-tla (add1 lemming-x) lemming-x)
+   (if dotaknil-tla lemming-y (add1 lemming-y))))
 
 (big-bang
-  (stanje-igre 0 pada)
+  (stanje-igre 0 pada 262 20)
   (on-tick naslednji 0.12)
   (to-draw animiraj))
